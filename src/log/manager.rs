252a4c46bbd638f294b2b92adbbb0b6681c08834
Ryuta339 <soccer.271828.339@gmail.com>
2023-09-30 00:19:06 +0900

    test: log test

--- src/log/manager.rs
+++ src/log/manager.rs
@@ -8,6 +8,8 @@ use crate::file::block_id::BlockId;
 use crate::file::manager::FileMgr;
 use crate::file::page::Page;
 
+use super::iterator::LogIterator;
+
 #[derive(Debug)]
 enum LogMgrError {
 	LogPageAccessFailed,
@@ -17,7 +19,7 @@ impl std::error::Error for LogMgrError {}
 impl fmt::Display for LogMgrError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
-			LogMgrError::LogPageAccessFailed => write!(f, "TODO"),
+			LogMgrError::LogPageAccessFailed => write!(f, "log access failed"),
 		}
 	}
 }
@@ -72,6 +74,21 @@ impl LogMgr {
 		Ok(logmgr)
 	}
 
+	pub fn iterator(&mut self) -> Result<LogIterator> {
+		self.flush_to_fm()?;
+		let iter = LogIterator::new(Arc::clone(&self.fm), self.current_blk.clone())?;
+
+		Ok(iter)
+	}
+
+	pub fn flush(&mut self, lsn: u64) -> Result<()> {
+		if lsn > self.last_saved_lsn {
+			self.flush_to_fm()?;
+		}
+
+		Ok(())
+	}
+
 	pub fn append(&mut self, logrec: &mut Vec<u8>) -> Result<u64> {
 		if self.l.lock().is_ok() {
 			let mut boundary = self.logpage.get_i32(0)?;
@@ -80,7 +97,7 @@ impl LogMgr {
 			let bytes_needed = recsize + int32_size;
 
 			if boundary - bytes_needed < int32_size {
-				self.flush()?;
+				self.flush_to_fm()?;
 
 				self.current_blk = self.append_newblk()?;
 				boundary = self.logpage.get_i32(0)?;
@@ -97,15 +114,7 @@ impl LogMgr {
 		Err(From::from(LogMgrError::LogPageAccessFailed))
 	}
 
-	pub fn flush_from_lsn(&mut self, lsn: u64) -> Result<()> {
-		if lsn > self.last_saved_lsn {
-			self.flush()?;
-		}
-
-		Ok(())
-	}
-
-	fn flush(&mut self) -> Result<()> {
+	fn flush_to_fm(&mut self) -> Result<()> {
 		self.fm
 			.borrow_mut()
 			.write(&self.current_blk, &mut self.logpage)?;
@@ -121,5 +130,62 @@ impl LogMgr {
 		self.fm.borrow_mut().write(&blk, &mut self.logpage)?;
 
 		Ok(blk)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::file::manager::FileMgr;
+
+	static LOG_FILE: &str = "simbledb.log";
+
+	#[test]
+	fn log_test() {
+		let fm = FileMgr::new("logtest", 400).unwrap();
+		let mut lm = LogMgr::new(
+			Arc::new(RefCell::new(fm)),
+			LOG_FILE
+			).unwrap();
+		let _ = create_records(&mut lm, 1, 35);
+		let _ = print_log_records(&mut lm, "The log file now has these: records:");
+		let _ = create_records(&mut lm, 36, 70);
+		let _ = lm.flush(65);
+		let _ = print_log_records(&mut lm, "The log file now has these records:");
+	}
+
+	fn print_log_records(lm: &mut LogMgr, msg: &str) -> Result<()> {
+		println!("{}", msg);
+		let iter = lm.iterator()?;
+		for rec in iter {
+			let p = Page::new_from_bytes(rec);
+			let s = p.get_string(0).unwrap();
+			let npos = Page::max_length(s.len());
+			let val = p.get_i32(npos).unwrap();
+			println!("[{}, {}]", s, val);
+		}
+		println!();
+
+		Ok(())
+	}
+
+	fn create_records(lm: &mut LogMgr, start: i32, end: i32) -> Result<()> {
+		println!("Creating records:");
+		for i in start..(end+1) {
+			let mut rec = create_log_record(format!("record{}", i).as_str(), i+100)?;
+			let lsn = lm.append(&mut rec)?;
+			println!("{} ", lsn);
+		}
+		println!();
+		Ok(())
+	}
+
+	fn create_log_record(s: &str, n: i32) -> Result<Vec<u8>> {
+		let npos = Page::max_length(s.len());
+		let b = Vec::<u8>::with_capacity(npos + 32);
+		let mut p = Page::new_from_bytes(b);
+		let _ = p.set_string(0, s.to_string())?;
+		let _ = p.set_i32(npos, n)?;
+		Ok(p.contents().clone())
 	}
 }
