79de95dae326c85b238e7d3706141708a35bb09b
Ryuta339 <soccer.271828.339@gmail.com>
2023-10-08 03:39:42 +0900

    test: buffer test but no assertion

--- src/buffer/buffer.rs
+++ src/buffer/buffer.rs
@@ -108,3 +108,50 @@ impl Buffer {
 		self.pins -= 1;
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::file::{block_id::BlockId, manager::FileMgr};
+	use crate::log::manager::LogMgr;
+	use crate::buffer::manager::BufferMgr;
+
+	static LOG_FILE: &str = "simpledb.log";
+
+	#[test]
+	fn buffer_test() {
+		let fm = FileMgr::new("buffertest", 400).unwrap();
+		let fm_arc = Arc::new(RefCell::new(fm));
+		let lm = LogMgr::new(Arc::clone(&fm_arc), LOG_FILE).unwrap();
+		let lm_arc = Arc::new(RefCell::new(lm));
+		let mut bm = BufferMgr::new(fm_arc, lm_arc, 3);
+
+		let buff1 = bm.pin(&BlockId::new("testfile", 1)).unwrap();
+		{
+			// In this block, buff1 is borrowed and cannot be used
+			let mut b1 = buff1.borrow_mut();
+			let p = b1.contents();
+			let n = p.get_i32(80).unwrap();
+			let _ = p.set_i32(80, n+1);
+			b1.set_modified(1, 0);
+			println!("The new value is {}", n + 1);
+		}
+		let _ = bm.unpin(buff1);
+
+		// One of these pins will flush buff1 to disk:
+		let buff2 = bm.pin(&BlockId::new("testfile", 2)).unwrap();
+		let _buff3 = bm.pin(&BlockId::new("testfile", 3)).unwrap();
+		let _buff4 = bm.pin(&BlockId::new("testfile", 4)).unwrap();
+
+		let _ = bm.unpin(buff2);
+		let buff2 = bm.pin(&BlockId::new("testfile", 1)).unwrap();
+		{
+			// In this block, buff2 is borrowed and cannot be used
+			let mut b2 = buff2.borrow_mut();
+			let p = b2.contents();
+			let _ = p.set_i32(80, 9999);
+			b2.set_modified(1, 0);
+		}
+		let _ = bm.unpin(buff2);
+	}
+}
