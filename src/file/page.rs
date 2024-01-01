d207ef77424e61bf85872fb2b3b814787a8c85d4
Ryuta339 <soccer.271828.339@gmail.com>
2024-01-01 18:51:23 +0900

    refactor: commonalized set method in Page

--- src/file/page.rs
+++ src/file/page.rs
@@ -17,8 +17,40 @@ impl fmt::Display for PageError {
 	}
 }
 
+pub trait ToPageBytes: Sized {
+	fn to_page_bytes(&self) -> Vec<u8>;
+}
+impl ToPageBytes for i32 {
+	fn to_page_bytes(&self) -> Vec<u8> {
+		self.to_be_bytes().to_vec()
+	}
+}
+impl ToPageBytes for &[u8] {
+	fn to_page_bytes(&self) -> Vec<u8> {
+		let mut v = (self.len() as i32).to_page_bytes();
+		v.append(&mut self.to_vec());
+		v
+	}
+}
+impl ToPageBytes for String {
+	fn to_page_bytes(&self) -> Vec<u8> {
+		self.as_bytes().to_page_bytes()
+	}
+}
+
+trait Setter<T: ToPageBytes> {
+	fn set (&mut self, offset: usize, t: T) -> Result<usize>;
+}
+
 pub struct Page {
 	bb: Vec<u8>,
+}
+
+impl<T: ToPageBytes> Setter<T> for Page {
+	fn set (&mut self, offset: usize, t: T) -> Result<usize> {
+		let bytes = t.to_page_bytes();
+		self.set_page_bytes(offset, bytes)
+	}
 }
 
 impl Page {
@@ -29,6 +61,18 @@ impl Page {
 	pub fn new_from_size(blocksize: usize) -> Self {
 		Self {
 			bb: vec![0u8; blocksize],
+		}
+	}
+
+	fn set_page_bytes(&mut self, offset: usize, b: Vec<u8>) -> Result<usize> {
+		let size = b.len();
+		if offset + size - 1 < self.bb.len() {
+			for (p, added) in izip!(&mut self.bb[offset..offset+size], &b) {
+				*p = *added;
+			}
+			Ok(offset + size)
+		} else {
+			Err(PageError::BufferSizeExceeded.into())
 		}
 	}
 
@@ -44,17 +88,7 @@ impl Page {
 	}
 
 	pub fn set_i32(&mut self, offset: usize, n: i32) -> Result<usize> {
-		let bytes = n.to_be_bytes();
-
-		if offset + bytes.len() - 1 < self.bb.len() {
-			for (b, added) in izip!(&mut self.bb[offset..offset + bytes.len()], &bytes) {
-				*b = *added;
-			}
-
-			Ok(offset + bytes.len())
-		} else {
-			Err(PageError::BufferSizeExceeded.into())
-		}
+		self.set(offset, n)
 	}
 
 	pub fn get_bytes(&self, offset: usize) -> Result<&[u8]> {
@@ -69,16 +103,7 @@ impl Page {
 	}
 
 	pub fn set_bytes(&mut self, offset: usize, b: &[u8]) -> Result<usize> {
-		if offset + mem::size_of::<i32>() + b.len() - 1 < self.bb.len() {
-			let new_offset = self.set_i32(offset, b.len() as i32)?;
-			for (p, added) in izip!(&mut self.bb[new_offset..new_offset + b.len()], b) {
-				*p = *added
-			}
-
-			Ok(new_offset + b.len())
-		} else {
-			Err(PageError::BufferSizeExceeded.into())
-		}
+		self.set(offset, b)
 	}
 
 	pub fn get_string(&self, offset: usize) -> Result<String> {
@@ -89,7 +114,7 @@ impl Page {
 	}
 
 	pub fn set_string(&mut self, offset: usize, s: String) -> Result<usize> {
-		self.set_bytes(offset, s.as_bytes())
+		self.set(offset, s)
 	}
 
 	pub fn max_length(strlen: usize) -> usize {
