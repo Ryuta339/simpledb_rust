71c0f7847e603864ca8c5a5690b6c8ccb1759234
Ryuta339 <soccer.271828.339@gmail.com>
2024-01-21 01:37:19 +0900

    feat: implement ConcurrencyMgr

--- src/tx/concurrency/manager.rs
+++ src/tx/concurrency/manager.rs
@@ -1,5 +1,7 @@
+use anyhow::Result;
 use std::{
 	collections::HashMap,
+	net::ToSocketAddrs,
 	sync::{Arc, LockResult, Mutex, Once}
 };
 
@@ -29,6 +31,36 @@ impl ConcurrencyMgr {
 				locks: HashMap::new(),
 			}
 		}
+	}
+
+	pub fn s_lock(&mut self, blk: &BlockId) -> Result<()> {
+		if self.locks.get(&blk).is_none() {
+			self.locktbl.lock().unwrap().s_lock(blk)?;
+			self.locks.insert(blk.clone(), "S".to_string());
+		}
+
+		Ok(())
+	}
+	pub fn x_lock(&mut self, blk: &BlockId) -> Result<()> {
+		if !self.has_x_lock(blk) {
+			self.s_lock(blk)?;
+			self.locktbl.lock().unwrap().x_lock(blk)?;
+			self.locks.insert(blk.clone(), "X".to_string());
+		}
+
+		Ok(())
+	}
+	pub fn release(&mut self) -> Result<()> {
+		for blk in self.locks.keys() {
+			self.locktbl.lock().unwrap().unlock(blk)?;
+		}
+		self.locks.clear();
+
+		Ok(())
+	}
+	fn has_x_lock(&self, blk: &BlockId) -> bool {
+		let locktype = self.locks.get(blk);
+		locktype.is_some() && locktype.unwrap().eq("X")
 	}
 }
 
